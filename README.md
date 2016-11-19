20dd550e6117d6bc770f24b9108a7b709194062c
beMaddz <madhu05ghosh@gmail.com>
2016-11-19 11:58:36 +0530

    Changing the Object models

---
+++ README.md
@@ -0,0 +1,8 @@
+FOR the data input via terminal.........
+
+using node.js (process.argv[])
+
+Input line contains:
+nodejs fileName.js totalNumberOfInputs WeatherName FirstOrbitName SecondOrbitName FirstOrbitNameTraffic SecondOrbitNameTraffic
+
+eg: xyz~$ nodejs main.js 5 Sunny Orbit1 Orbit2 12 10
--- calculation/bestVehicle.js
+++ calculation/bestVehicle.js
@@ -7,46 +7,49 @@ var dataWeather = require('../lib/Weather.js');
 var dataOrbit = require('../lib/Orbits.js');
 //import the orbit detais......
 
+var dataVehicle = require('../lib/Vehicles.js')
+//import the vehicle details......
+
 var weatherConditions = dataWeather.getWeather();
 var orbitCollection = dataOrbit.getOrbits();
+var vehicleCollection = dataVehicle.getVehicles();
 
-function craterCalculation(craters, weather, changeInCrater) {
-  var newCraterNum = 0;
-  if(weather === 'Sunny') {
-    newCraterNum = craters - (changeInCrater * craters);
-  }
-  else {
-    newCraterNum = craters + (changeInCrater * craters);
-  }
-  return newCraterNum;
-}
-//calculating the number of craters......
-
-
-function vehicleTime(vehicle, orbit, crater) {
+function leastVehicleTime(vehicle, orbit, weather) {
   var vehicleFastest = {};
   var leastTime = Infinity;
   var fastVehicle = '';
-  for (var v = 0; v < vehicle.length; v++) {
-    var travelSpeed = vehicle[v].vehicleSpeed >= orbit.orbitTraffic ? orbit.orbitTraffic : vehicle[v].vehicleSpeed;
-    var time = (orbit.orbitDistance / travelSpeed) + ((vehicle[v].vehicleTimePerCrater * crater) / 60);
-    fastVehicle = leastTime > time ? vehicle[v].vehicleName : fastVehicle;
-    leastTime = leastTime > time ? time : leastTime;
+  for(var t in vehicle) {
+    var checkVehicle = vehicle[t];
+    for(var w = 0; w < weather.availableTransport.length; w++) {
+      if(checkVehicle.vehicleName === weather.availableTransport[w]) {
+
+        checkVehicle.VehicleTime(orbit, weather);
+        //modifying the time taking by vehicles in a particular weather..........
+
+      }
+    }
+    fastVehicle = leastTime > checkVehicle.time ? checkVehicle.vehicleName : fastVehicle;
+    leastTime = leastTime > checkVehicle.time ? checkVehicle.time : leastTime;
   }
   vehicleFastest.fastName = fastVehicle;
   vehicleFastest.fastTime = leastTime;
   return vehicleFastest;
 }
-//calculates the fastest vehicle and returns the vehicle details......
+// function for the fastest vehicle......
 
 
 expoFastVehicle.perfectVehicle = function(orbitName, trafficSpeed, weather) {
   var inputOrbit = orbitCollection[orbitName];
+
   inputOrbit.orbitTraffic = trafficSpeed;
+  //changing the orbit traffic with the input......
+
   var inputWeather = weatherConditions[weather];
-  var vehicle = inputWeather.availableTransport;
-  var totalCraters = craterCalculation(inputOrbit.numberOfCraters, inputWeather.weatherName, inputWeather.craterNumChange);
-  var chosenVehicle = vehicleTime(vehicle, inputOrbit, totalCraters);
+
+  inputOrbit.craterCalculation(inputWeather);
+  // modifying the number of craters according to weather.....
+
+  var chosenVehicle = leastVehicleTime(vehicleCollection, inputOrbit, inputWeather);
   return chosenVehicle;
 }
 //exports the fastest vehicle details......
